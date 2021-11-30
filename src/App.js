fd31f77d9cbc43b3cf479c299b4b30be0159326e
Cintya01 <cintya.olivares.c@gmail.com>
2021-11-30 17:13:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {firestore} from './firebase';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [msn, setMsn] =useState ([])
+  
+  useEffect (() => {
+    firestore.collection("tweets")
+    .get()
+    .then ((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+        setMsn(doc.data());
+      });
+    });
+
+  },[msn])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+         <p>{msn.tweet}</p>
       </header>
     </div>
   );
---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSANceXG3slD35JE30Wjk0Qvsb-Tj2oyY",
+    authDomain: "proyecto-s4-acamica.firebaseapp.com",
+    projectId: "proyecto-s4-acamica",
+    storageBucket: "proyecto-s4-acamica.appspot.com",
+    messagingSenderId: "796262028821",
+    appId: "1:796262028821:web:3c8ad69cc39a58798e201c"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// Exporta la funcionalidad de la DB
+export const firestore = firebase.firestore()
+// exporta el paquete de firebase para poder usarlo
+export default firebase
