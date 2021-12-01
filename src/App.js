055438e4f9d4fdeadc445ecb570a3b7915baba00
Cintya01 <cintya.olivares.c@gmail.com>
2021-12-01 13:06:29 -0300

    Header Basic Config

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 // import {firestore} from './firebase';
 import React, { useState} from 'react';
 import Twitter from './Tweets';
+import Header from './Header';
 
 function App() {
 
   const [msn, setMsn] =useState ([])
   
   return (
-    <div className="App">      
+    <div className="App">  
+      <Header/>
        <Twitter tweets={msn} setTweets={setMsn}/>
      </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <div className="header-cont flex">
+            <nav className="header-nav-cont flex">
+                <div>img user</div>
+                <div>logo</div>
+                <div>nombre</div>
+            </nav>
+        </div>
+    )
+    
+}
+
+export default Header;
